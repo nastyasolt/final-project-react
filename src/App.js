93b13b88b3643519ac997a3b31fa30d86fd256ad
nastyasolt <nastyasolt1988@gmail.com>
2022-10-28 15:40:57 -0400

    added draft of Weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import './App.css';
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      
+      <Weather />
     <footer>
       This project was coded by Anastasiia Soltysiak and is {" "}
        <a href="https://github.com/nastyasolt/final-project-react" 
