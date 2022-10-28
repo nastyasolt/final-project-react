a5a609d7b648ed5c4043ff04bbf750fdbbb51a16
nastyasolt <nastyasolt1988@gmail.com>
2022-10-28 15:21:39 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
     <footer>
       This project was coded by Anastasiia Soltysiak and is {" "}
@@ -13,6 +14,7 @@ export default function App() {
         open sourced on GitHub
        </a>
     </footer>
+    </div>
     </div>
   );
 }
