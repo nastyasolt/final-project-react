537b8d9f592b58731988a09a5fbc903ee9ff5c6e
nastyasolt <nastyasolt1988@gmail.com>
2022-10-28 15:15:44 -0400

    added GitHub Url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+    <footer>
+      This project was coded by Anastasiia Soltysiak and is {" "}
+       <a href="https://github.com/nastyasolt/final-project-react" 
+       target="_blank" 
+       rel="noreferrer">
+        open sourced on GitHub
+       </a>
+    </footer>
+    </div>
   );
 }
 
